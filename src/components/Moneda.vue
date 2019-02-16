65ef4afbffd0ba8cc3267e856a859814e46812dd
nruizg <nicolasruiz81@gmail.com>
2019-02-16 17:16:40 -0300

    diferenciacion de monedass

--- src/components/Moneda.vue
+++ src/components/Moneda.vue
@@ -3,9 +3,9 @@
     <div class="component" :style="{backgroundColor: randomColor()}">
       <h3>{{ moneda.nombre }}</h3>
       
-        <h5 v-if="moneda.unidad_medida == 'Pesos'">Valor en {{ moneda.unidad_medida }} Chilenos: {{moneda.valor}}CLP</h5>
-        <h5 v-else-if="moneda.unidad_medida == 'Porcentaje'">Valor en {{ moneda.unidad_medida }} Chilenos: {{moneda.valor}}%</h5>
-        <h5 v-else>Valor en {{ moneda.unidad_medida }} Chilenos: {{moneda.valor}}USD</h5>
+        <h5 v-if="moneda.unidad_medida == 'Pesos'">Valor en {{ moneda.unidad_medida }} Chilenos: {{moneda.valor}} CLP</h5>
+        <h5 v-else-if="moneda.unidad_medida == 'Porcentaje'">Valor en {{ moneda.unidad_medida }}: {{moneda.valor}}%</h5>
+        <h5 v-else>Valor en {{ moneda.unidad_medida }} Chilenos: {{moneda.valor}} USD</h5>
     </div>
   </div>
 </template>
